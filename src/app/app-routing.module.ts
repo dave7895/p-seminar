cab4238b6c249c010f6249351ec00c20ad69d9e9
Amelie <amelie.strupp0@gmail.com>
2020-10-16 08:45:50 +0200

    Have added a first draft for text page and contents page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
+import { TextPageComponent } from './pages/text-page/text-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContentsPageComponent } from './pages/contents-page/contents-page.component';
 
 
 const routes: Routes = [
-  {path: 'contents', component: ContentsPageComponent}
+  {path: 'contents', component: ContentsPageComponent},
+  {path: 'text', component: TextPageComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,38 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContentsPageComponent } from './pages/contents-page/contents-page.component';
+import { TextPageComponent } from './pages/text-page/text-page.component';
+import { HeaderComponent } from './components/structure/header/header.component';
+// Used to display the dialogs to show the translation of words
+import {MatDialogModule} from '@angular/material/dialog';
+// Used for buttons
+import {MatButtonModule} from '@angular/material/button';
+// Used to display material icons
+import {MatIconModule} from '@angular/material/icon';
+import { ShowTranslationDialogComponent } from './components/dialogs/show-translation-dialog/show-translation-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContentsPageComponent
+    ContentsPageComponent,
+    TextPageComponent,
+    HeaderComponent,
+    ShowTranslationDialogComponent,
+    TextPageComponent,
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
